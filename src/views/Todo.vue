febd99aecd248df594962ed52b31822bdf5114fb
E-K8 <ekatplat@gmail.com>
2022-12-05 20:11:48 +0000

    add dummy data with dividers

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <v-list flat>
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active" color="primary"></v-checkbox>
-          </v-list-item-action>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>My first todo</v-list-item-title>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
     </v-list>
   </div>
 </template>
@@ -19,5 +22,14 @@
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: 'open your eyes' },
+        { id: 2, title: 'yawn' },
+        { id: 3, title: 'stretch' },
+      ],
+    };
+  },
 };
 </script>
