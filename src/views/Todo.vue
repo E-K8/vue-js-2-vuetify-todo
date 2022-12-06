ce356dfa079733f2b57a79e3e80d3f632d781f4d
E-K8 <ekatplat@gmail.com>
2022-12-06 15:44:15 +0000

    mark tasks as done: tick checkbox, add strike-through, add bg colour

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,14 +2,21 @@
   <div class="home">
     <v-list flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item>
-          <template v-slot:default="{ active }">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'teal lighten-5': task.done }"
+        >
+          <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+              >
+                {{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -25,11 +32,19 @@ export default {
   data() {
     return {
       tasks: [
-        { id: 1, title: 'open your eyes' },
-        { id: 2, title: 'yawn' },
-        { id: 3, title: 'stretch' },
+        { id: 1, title: 'open your eyes', done: false },
+        { id: 2, title: 'yawn', done: false },
+        { id: 3, title: 'stretch', done: false },
       ],
     };
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      console.log(task);
+
+      task.done = !task.done;
+    },
   },
 };
 </script>
