cfabb3d483e22e3b68f91db6db002cbfca975d84
E-K8 <ekatplat@gmail.com>
2022-12-13 16:54:25 +0000

    create child component Task

--- src/components/Todo/ListTasks.vue
+++ src/components/Todo/ListTasks.vue
@@ -1,36 +1,17 @@
 <template>
   <v-list class="pt-0" flat>
-    <div v-for="task in $store.state.tasks" :key="task.id">
-      <v-list-item
-        @click="$store.commit('doneTask', task.id)"
-        :class="{ 'teal lighten-5': task.done }"
-      >
-        <template v-slot:default>
-          <v-list-item-action>
-            <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title
-              :class="{ 'text-decoration-line-through': task.done }"
-            >
-              {{ task.title }}
-            </v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-btn @click.stop="$store.commit('deleteTask', task.id)" icon>
-              <v-icon color="#5AA19B">mdi-delete-outline</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </template>
-      </v-list-item>
-      <v-divider></v-divider>
-    </div>
+    <task v-for="task in $store.state.tasks" :key="task.id" :task="task" />
   </v-list>
 </template>
 
 <script>
-export default {};
+import TaskVue from '@/components/Todo/Task.vue';
+
+export default {
+  components: {
+    task: TaskVue,
+  },
+};
 </script>
 
 <style></style>
